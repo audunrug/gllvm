eb977a64adc8a47095e047b43075be75fa6a9e2d
Audun Rugstad <77783308+audunrug@users.noreply.github.com>
2025-05-20 23:00:51 +0200

    add rotate and scare options to getLoadings + minor cleanup

--- R/getLV.gllvm.R
+++ R/getLV.gllvm.R
@@ -3,6 +3,8 @@
 #' 
 #' @param object an object of class 'gllvm'.
 #' @param type type of latent variable scores to retrieve from a gllvm object. For models with unconstrained latent variables, defaults to "residual". For models with constrained latent variables, defaults to conditional. Alternatively, "marginal" returns linear combination scores without residual error.
+#' @param rotate whether to rotate the latent variable scores based on their singular value decomposition 
+#' @param scale whether to scale the latent variable scores in accordance with the scaling rules of the "ordiplot" function
 #' @param ... not used
 #' 
 #' @details
@@ -84,9 +86,9 @@ getLV.gllvm <- function(object, rotate = FALSE, scale = FALSE, type = NULL, ...)
     lvs <- lvs %*% svd_rotmat_sites # rotate lvs NB: does the scaling affect the svd??
   }
   
-  if (scale) {
+  if (scale) { # scale using Bert's mystery formula
     # get species loadings scaled by sigma.lv
-    choose.lv.coefs <- getLoadings.gllvm(object)
+    choose.lv.coefs <- getLoadings(object)
     
     bothnorms <- vector("numeric",ncol(choose.lv.coefs))
     for(i in 1:ncol(choose.lv.coefs)){
@@ -95,10 +97,6 @@ getLV.gllvm <- function(object, rotate = FALSE, scale = FALSE, type = NULL, ...)
     
     ## Standardize both to unit norm then scale using bothnorms. Note alpha = 0.5 so both have same norm. Otherwise "significance" becomes scale dependent
     scaled_cw_sites <- t(t(lvs) / sqrt(colSums(lvs^2)) * (bothnorms^0.5)) 
-    scaled_cw_species <- choose.lv.coefs
-    for(i in 1:ncol(scaled_cw_species)){
-      scaled_cw_species[,i] <- choose.lv.coefs[,i] / sqrt(sum(choose.lv.coefs[,i]^2)) * (bothnorms[i]^(1-0.5)) 
-    }
     
     lvs <- scaled_cw_sites
   }
--- R/getLoadings.gllvm.R
+++ R/getLoadings.gllvm.R
@@ -2,6 +2,8 @@
 #' @description  Extract loadings (species scores) from a gllvm object.
 #' 
 #' @param object an object of class 'gllvm'.
+#' @param rotate whether to rotate the loadings based on the singular value decomposition of the site scores
+#' @param scale whether to scale the loadings in accordance with the scaling rules of the "ordiplot" function
 #' @param ... not used
 #' 
 #' @details
@@ -11,13 +13,33 @@
 #'@export
 #'@export getLoadings.gllvm
 
-getLoadings.gllvm <- function(object, ...)
+getLoadings.gllvm <- function(object, scale = FALSE, rotate = FALSE, ...)
 {
   if(inherits(object, "gllvm.quadratic"))stop("For the quadratic response model, please use 'optima.gllvm' instead.")
   sploads <- object$params$theta
   
   if(object$num.lv>0)sploads[,(ncol(sploads)-object$num.lv+1):ncol(sploads)] <- sploads[,(ncol(sploads)-object$num.lv+1):ncol(sploads)]%*%diag(tail(object$params$sigma.lv, object$num.lv))
   # if(object$num.lv.c>0 & scale)sploads[,1:object$num.lv.c] <- sploads[,1:object$num.lv.c]%*%diag(head(object$params$sigma.lv, object$num.lv.c))
+  
+  if (rotate) {
+    svd_rotmat <- svd(getLV.gllvm(object))$v # rotation matrix for sites
+    sploads <- sploads %*% svd_rotmat # rotate loadings
+  }
+  
+  if (scale) { # scale using Bert's mystery formula
+    bothnorms <- vector("numeric",ncol(sploads))
+    for(i in 1:ncol(sploads)){
+      bothnorms[i] <- sqrt(sum(lvs[,i]^2)) * sqrt(sum(sploads[,i]^2))
+    }
+    
+    ## Standardize to unit norm then scale using bothnorms. Note alpha = 0.5 so both have same norm. Otherwise "significance" becomes scale dependent
+    scaled_cw_species <- sploads
+    for(i in 1:ncol(scaled_cw_species)){
+      scaled_cw_species[,i] <- sploads[,i] / sqrt(sum(sploads[,i]^2)) * (bothnorms[i]^(1-0.5)) 
+    }
+    
+    sploads <- scaled_cw_species
+  }
   
   return(sploads)
 }
